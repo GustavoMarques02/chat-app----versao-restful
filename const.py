81fc385436df5580965a3e5d1ac2230245570058
Gustavo Marques Reis <gustavo_marquesr@discente.ufg.br>
2022-08-07 15:12:44 -0300

    Criando arquivos

---
+++ const.py
@@ -0,0 +1,5 @@
+CHAT_SERVER_HOST = "172.31.82.61"
+CHAT_SERVER_PORT = "5001"
+
+# IDs de usuários registrados com os respectivos endereços IP e números de porta
+registry = {"Alice":("172.31.95.205","5002"), "Bob":("172.31.89.221","5002"), "Charlie":("172.31.84.121", "5002")}
