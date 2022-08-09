4996f6b90f58242f580711e8759ec945c8f0d459
Gustavo Marques Reis <gustavo_marquesr@discente.ufg.br>
2022-08-09 10:14:24 -0300

    Update chatserver.py

--- chatserver.py
+++ chatserver.py
@@ -1,15 +1,16 @@
-from crypt import methods
 from flask import Flask
 from flask import request
-
 import const
 
 app = Flask(__name__)
 
-chatBD=[]
+chatsWaitingBD=[]
+chatsRelayingBD=[]
 
 @app.route('/chat/message', methods=['POST'])
 def sendMessage():
+    print("RELAYING MSG: " + request.json['text'] + " - FROM: " + request.json['nameSender'] + 
+        " - TO: " + request.json['nameDestination'])
     try:
         const.registry[request.json['nameDestination']]
     except:
@@ -20,21 +21,25 @@ def sendMessage():
             'nameSender':request.json['nameSender'],
             'nameDestination':request.json['nameDestination']
         }
-        chatBD.append(dat)
+        chatsWaitingBD.append(dat)
         return True
 
 @app.route('/chat/message', methods=['GET'])
 def relayMessage():
     while True:
-        auxChat = chatBD
-        for c in chatBD:
+        auxChat = chatsWaitingBD
+        for c in chatsWaitingBD:
+            chatsWaitingBD.remove(c)
+            chatsRelayingBD.append(c)
+        auxChat = chatsRelayingBD
+        for c in chatsRelayingBD:
             dest_addr = const.registry[c['nameDestination']]
             dest_ip = dest_addr[0]
             dest_port = dest_addr[1]
             if dest_ip == request.json['ip'] and dest_port == request.json['port']:
                     auxChat.remove(c)
                     yield c
-        chatBD = auxChat
+        chatsRelayingBD = auxChat
 
 if __name__ == '__main__':
     app.run()
