7e68a9bde1648a9c188007eb0c3360f1a15e9287
Gustavo Marques Reis <gustavo_marquesr@discente.ufg.br>
2022-08-13 19:59:04 -0300

    Update chatserver.py

--- chatserver.py
+++ chatserver.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -7,6 +8,7 @@ app = Flask(__name__)
 
 chatsWaitingBD=[]
 chatsRelayingBD=[]
+chatsSentBD=[]
 
 @app.route('/chat/message', methods=['POST'])
 def sendMessage():
@@ -18,10 +20,16 @@ def sendMessage():
         return 'NOT OK'
     else:
         dat = {
+            'number': request.json['number'],
             'text': request.json['text'],
             'nameSender': request.json['nameSender'],
-            'nameDestination': request.json['nameDestination']
+            'nameDestination': request.json['nameDestination'],
         }
+        if request.json['numberReply'] == 0:
+            dat['messageReply'] = ''
+        else:
+            aux = [chat for chat in chatsSentBD if (chat['number'] == request.json['numberReply'])]
+            dat['messageReply'] = aux['text']
         chatsWaitingBD.append(dat)
         return 'OK'
 
@@ -38,8 +46,9 @@ def relayMessage():
             dest_ip = dest_addr[0]
             dest_port = dest_addr[1]
             if dest_ip == request.json['ip'] and dest_port == request.json['port']:
-                    chatsRelayingBD.remove(chat)
-                    return chat
+                chatsRelayingBD.remove(chat)
+                chatsSentBD.append(chat)
+                return chat
 
 if __name__ == '__main__':
     app.run(host=const.CHAT_SERVER_HOST, port=const.CHAT_SERVER_PORT)
